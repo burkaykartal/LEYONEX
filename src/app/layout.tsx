b22d1b258c26496729a4eac1ccb5326f984fdee7
burkaykartal <agnathe@gmail.com>
2026-04-20 12:50:32 +0300

    analytics update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import { ClerkProvider } from '@clerk/nextjs';
 import { trTR } from '@clerk/localizations';
 import GoogleTranslate from '@/components/GoogleTranslate';
 import "./globals.css";
+// Next.js Script bileşenini içe aktarıyoruz
+import Script from "next/script"; 
 
 const poppins = Poppins({
   weight: ["300", "400", "500", "600", "700"],
@@ -26,6 +28,23 @@ export default function RootLayout({
         <body className={poppins.className}>
           <GoogleTranslate />
           {children}
+
+          {/* Google Analytics Kodları Başlangıcı */}
+          <Script 
+            src="https://www.googletagmanager.com/gtag/js?id=G-BS3L40XBTH" 
+            strategy="afterInteractive" 
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-BS3L40XBTH');
+            `}
+          </Script>
+          {/* Google Analytics Kodları Bitişi */}
+          
         </body>
       </html>
     </ClerkProvider>
