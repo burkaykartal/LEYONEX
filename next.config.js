cc81c553b4659f14c4e734e84f38a4bc2ba5367f
burkaykartal <agnathe@gmail.com>
2026-04-30 16:14:40 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,20 @@ const nextConfig = {
       { protocol: 'https', hostname: 'cdn.sanity.io' },
     ],
   },
+  async headers() {
+    return [
+      {
+        // Tüm sayfalara uygulanması için (.*) kullanıyoruz
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'index, follow', // noindex'i zorla ezecek olan sihirli kelime
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
